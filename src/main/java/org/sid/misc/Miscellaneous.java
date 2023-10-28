467d082b230427dcfd26f346279e37a534cd8ea7
Siddharth Kulkarni <sidd.kulk@gmail.com>
2023-10-28 17:35:15 +0530

    Priority Queue implementation for Complex numbers with custom comparison logic

--- src/main/java/org/sid/misc/Miscellaneous.java
+++ src/main/java/org/sid/misc/Miscellaneous.java
@@ -5,10 +5,12 @@ import org.jetbrains.annotations.NotNull;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Base64;
+import java.util.Comparator;
+import java.util.PriorityQueue;
 
 public class Miscellaneous {
     public static void main(String[] args) throws NoSuchAlgorithmException {
-
+        CollectionsMisc.collections();
     }
 
     static byte[] sha256(@NotNull String input) throws NoSuchAlgorithmException {
@@ -17,7 +19,7 @@ public class Miscellaneous {
     }
 }
 
-record ComplexNumber(Double real, Double imaginary) {
+record ComplexNumber(Double real, Double imaginary) implements Comparable<ComplexNumber> {
     ComplexNumber add(ComplexNumber complexNumber) {
         if (complexNumber == null) return this;
 
@@ -27,4 +29,26 @@ record ComplexNumber(Double real, Double imaginary) {
     boolean isReal() {
         return this.imaginary == 0.0;
     }
+
+
+    @Override
+    public int compareTo(@NotNull ComplexNumber other) {
+        double thisMagnitude = Math.sqrt(this.real * this.real + this.imaginary * this.imaginary);
+        double otherMagnitude = Math.sqrt(other.real * other.real + other.imaginary * other.imaginary);
+
+        return Double.compare(thisMagnitude, otherMagnitude);
+    }
+}
+
+class CollectionsMisc {
+
+    public static void collections() {
+        PriorityQueue<ComplexNumber> pq = new PriorityQueue<>();
+        pq.add(new ComplexNumber(1.0d, 2.0d));
+        pq.add(new ComplexNumber(0.0d, 0.0d));
+        pq.add(new ComplexNumber(-1.0d, 9.0d));
+
+        System.out.println(pq.poll());
+    }
+
 }
