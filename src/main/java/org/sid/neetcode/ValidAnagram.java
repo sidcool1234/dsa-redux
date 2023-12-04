c679c9f88b7f3be4996dde483d7ec93ed27dde53
Siddharth Kulkarni <sidd.kulk@gmail.com>
2023-12-04 15:48:49 +0530

    Valid Anagram - OPtimized solution

--- src/main/java/org/sid/neetcode/ValidAnagram.java
+++ src/main/java/org/sid/neetcode/ValidAnagram.java
@@ -21,4 +21,13 @@ public class ValidAnagram {
 
         return String.valueOf(sChar).equals(String.valueOf(tChar));
     }
+
+    // O(n)
+    public boolean isAnagramOptimised(String s, String t) {
+        int[] alphabet = new int[26];
+        for (int i = 0; i < s.length(); i++) alphabet[s.charAt(i) - 'a']++;
+        for (int i = 0; i < t.length(); i++) alphabet[t.charAt(i) - 'a']--;
+        for (int i : alphabet) if (i != 0) return false;
+        return true;
+    }
 }
