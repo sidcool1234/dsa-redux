d771ac2d163e353972a0e3b3db1bf61c683f500c
Siddharth Kulkarni <sidd.kulk@gmail.com>
2023-12-29 09:46:22 +0530

    NC - Valid Palindrome

---
+++ src/main/java/org/sid/neetcode/ValidPalindrome.java
@@ -0,0 +1,23 @@
+package org.sid.neetcode;
+
+public class ValidPalindrome {
+    public boolean isPalindrome(String s) {
+        int i = 0, j = s.length() - 1;
+        while (i < j) {
+            while (i < j && !Character.isLetterOrDigit(s.charAt(i))) {
+                i++;
+            }
+
+            while (i < j && !Character.isLetterOrDigit(s.charAt(j))) {
+                j--;
+            }
+
+            if (Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(j)))
+                return false; // if they are not equal, the string is not a palindrome
+            i++; // move i forward
+            j--; // move j backwards
+        }
+
+        return true;
+    }
+}
